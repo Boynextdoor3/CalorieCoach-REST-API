73236f4b9700543f86aa3325a403b71be38c00de
MaxLisiy <3icestorm3@gmail.com>
2024-11-13 19:11:17 +0200

    Initial commit

---
+++ src/main/java/com/coursework/caloriecoach/CalorieCoachApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.caloriecoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieCoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalorieCoachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coursework/caloriecoach/CalorieCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.caloriecoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieCoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
